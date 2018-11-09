d595bc03f5fb0a96e8c8da4b69ae5d7027fe1d84
Brian Mock <mock@hpe.com>
2018-11-09 09:11:13 -0800

    adds bundle analyzer

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require("path");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
   entry: "./index",
-  mode: "development"
+  mode: "production",
+  devServer: {
+    compress: true
+  },
+  plugins: [new BundleAnalyzerPlugin()]
 };
