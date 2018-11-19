a474cad649b2cd4e25f4a736137508561f24dba4
marcsj <marc@marcsj.com>
2018-11-19 11:15:18 -0800

    Added swagger to proto generation.

--- proto/generate.sh
+++ proto/generate.sh
@@ -7,6 +7,7 @@ mkdir -p ../web/generated
 
 protoc -I ./ \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  --swagger_out=logtostderr=true:../backend/dog \
   --grpc-gateway_out=logtostderr=true:../backend/dog \
   --plugin=protoc-gen-ts=../web/node_modules/.bin/protoc-gen-ts \
   --js_out=import_style=commonjs,binary:../web/generated \
