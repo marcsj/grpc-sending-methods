12323469901eef7ccaaef4ef218e032732b0ff61
marcsj <marc@marcsj.com>
2018-11-30 12:27:50 -0800

    Added requests for deleting, adding, and getting all dogs.

--- backend/services/dog.go
+++ backend/services/dog.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"context"
 	"github.com/marcsj/grpc-sending-methods/backend/store"
 
 	"github.com/marcsj/grpc-sending-methods/backend/dog"
@@ -12,6 +13,9 @@ type dogTrackServer struct{
 
 type DogTrackServer interface {
 	TrackDogs(req *dog.TrackRequest, outStream dog.DogTrack_TrackDogsServer) error
+	AddDog(context.Context, *dog.Dog) (*dog.Response, error)
+	GetAllDogs(context.Context, *dog.TrackRequest) (*dog.ListDogs, error)
+	DeleteDog(context.Context, *dog.DeleteRequest) (*dog.Response, error)
 }
 
 func NewDogTrackServer(store store.DogStore) DogTrackServer {
@@ -39,4 +43,33 @@ func (s dogTrackServer) TrackDogs(req *dog.TrackRequest, outStream dog.DogTrack_
 		}
 	}
 	return nil
+}
+
+func (s dogTrackServer) AddDog(ctx context.Context, d *dog.Dog) (*dog.Response, error) {
+	err := s.store.AddDog(d)
+	if err != nil {
+		return nil, err
+	}
+	return &dog.Response{Message: "Dog successfully added."}, nil
+}
+
+func (s dogTrackServer) GetAllDogs(ctx context.Context, req *dog.TrackRequest) (*dog.ListDogs, error) {
+	locationTag := store.LocationTag{
+		LocationID: req.GetLocationId(),
+		FloorID: req.GetFloorId(),
+	}
+	dogs := s.store.GetAllDogs(locationTag)
+	return &dog.ListDogs{Dogs: dogs}, nil
+}
+
+func (s dogTrackServer) DeleteDog(ctx context.Context, req *dog.DeleteRequest) (*dog.Response, error) {
+	locationTag := store.LocationTag{
+		LocationID: req.GetLocationId(),
+		FloorID: req.GetFloorId(),
+	}
+	err := s.store.DeleteDog(locationTag, req.GetDogId())
+	if err != nil {
+		return nil, err
+	}
+	return &dog.Response{Message: "Dog successfully deleted."}, nil
 }
